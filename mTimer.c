4f494b24592003d98e5dae27ecd0235a2a712079
Roanne-KH <eng.Rowan.khaled1520@alexu.edu.eg>
2020-05-15 07:24:55 +0200

    5th Commit :< UART_INterfacing _with TIMER_ADC>

--- mTimer.c
+++ mTimer.c
@@ -3,13 +3,17 @@
 
 void Timer0_init(char TimerMode, char CompOUTMode, char Clk_S) {
 
-    //    switch()
+   //    switch()
     if (CompOUTMode) {
         DDRB |= (1 << 3); // PINB3 (OC0) as OUTPUT.
     }
 
     TCCR0 |= (TimerMode << 3) | (CompOUTMode << 4);
     TCCR0 |= (Clk_S << 0);
-    TIMSK |= (1 << OCIE0) | (1 << TOIE0); // Enable Interrupt OVF
-    
-}
+    //TIMSK |= (1 << OCIE0) | (1 << TOIE0); // Enable Interrupt OVF
+     TIMSK |=  (1 << TOIE0);
+   
+  /*  TCCR0 |=0x05;
+    TIMSK |=  (1 << TOIE0); // Enable Interrupt OVF
+
+   */}
--- newavr-main.c
+++ newavr-main.c
@@ -41,28 +41,59 @@ if (Rec == 'C') {   // CLOSE LED
        
      }
 }
-ISR(ADC_vect)
-    { 
-      char buffer[20];
-      int data =getADCdata();
-      itoa(data,buffer,10);
-      transmitString(buffer);
-      newline();
-      _delay_ms(1000);
-      startConv();
 
+   ISR(ADC_vect)
+   {/*
+    int val =getADCdata(); 
+    transmitValue(val*0.49);// Calculate the value in mV (val*4.9) , To convert mV to Temperature , as the sensor every 10mV --> 1C (val *.49))
+    transmitString(str3);   // This function takes pointer to char  
+    
+    //startConv();
+    */
+   }
 
+   ISR(TIMER0_OVF_vect)
+   {
+     static int _1Sec ;
+    _1Sec++;
+    if(_1Sec == 31){              // start ADC Conversion after 1 sec 
+        
+            
+        startConv();
+        while(!(ADCSRA &(1<<ADIF)));// ADIF IS set if ( ADIE =1 & sei()is called) when conversion is completed.
+        int val = getADCdata();
+       //Transmitting Temperature
+        int temp =0.49*val;
+        transmitValue(temp);// To convert mV to Temperature , as the sensor every 10mV --> 1C (val *.49))
+        //transmitString(str3);     
+        _1Sec=0;
+        if(temp>100){ //Turn LED ON
+            PORTC |= (1 << LED);
+            transmitString(str1);                
+        }
+        else         //Turn LED OFF
+        {
+         PORTC &=~ (1 << LED);
+         transmitString(str2);       
+         
+        }
+        newline();
     }
+    
+}
+    
 int main(void) {
      /* Replace with your application code */
     DDRC |= (1 << LED); // (LED pin)is set as Output
     UART_init(9600);    // Enable Transmitter ONLY
     ADC_init();
+    Timer0_init(NormalMode ,OC0_DIS ,CLK_io_PS1024);
 
     sei();              // Enable global interrupt
     startConv();
     
     while (1) {
 
-    }    }
+    }   
+}
 
